e207fd0d01cbd266a0f8322b009689e1c04dbaf6
I Putu Ananta Yogiswara <ananta.putu@gmail.com>
2015-07-24 19:59:25 +0700

    First Commit

--- client/templates/home.js
+++ client/templates/home.js
@@ -13,17 +13,17 @@ Template.home.rendered = function(){
 
 Template.home.destroyed = function() {
     console.log('Destroyed the home template');
-}
+};
 
 Template.home.helpers({
-    exampleHelper: function() {
-        //return "This text came from a helper with some <strong>HTML</strong>.";
-        return new Handlebars.SafeString('This text came from a helper with some <strong>HTML</strong>.');
-    },
-    dataContextHelper: function() {
-        return {
-            someText : 'This text was set using a helper of the parent template.',
-            someNested : 'That comes from "someNested.text"'
-        };
+    postsList: function() {
+        return Posts.find({}, {sort: {timeCreated: -1}});
+    }
+});
+
+Template.home.events({
+    'click button.lazyload': function(e, template) {
+        var currentLimit = Session.get('lazyloadLimit');
+        Session.set('lazyloadLimit', currentLimit + 2);
     }
 });
